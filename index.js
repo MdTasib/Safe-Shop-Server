a9db997c6226723e5f0ed2d8ab7dc25b1f1d34a2
MdTasib <ohidulalam442@gmail.com>
2021-09-01 12:50:06 +0600

    delete crud implement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello')
-})
+});
 
 const uri = "mongodb+srv://safeshop:safeshop@cluster0.do24a.mongodb.net/SafeShop?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,8 +21,22 @@ client.connect(err => {
   // post book on database
   app.post('/addBook', (req, res) => {
     const newBook = req.body;
-    console.log(newBook);
     collection.insertOne(newBook)
+      .then(result => res.send(result.acknowledged));
+  });
+
+  // get book on database
+  app.get('/books', (req, res) => {
+    collection.find({})
+      .toArray((error, document) => {
+        res.send(document);
+      });
+  });
+
+  // delete book on database
+  app.delete('/deleteBook/:id', (req, res) => {
+    const id = ObjectId(req.params.id);
+    collection.deleteOne({ _id: id })
       .then(result => res.send(result.acknowledged));
   });
 });
