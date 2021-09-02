3b72e9b7173a9d235e550897f51661c351d1ad5d
MdTasib <ohidulalam442@gmail.com>
2021-09-02 19:01:36 +0600

    done

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello')
 });
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.do24a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://safeshop:safeshop@cluster0.do24a.mongodb.net/SafeShop?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.do24a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -44,4 +43,4 @@ client.connect(err => {
 });
 
 const port = 5000;
-app.listen(port);
+app.listen(process.env.PORT || port);
