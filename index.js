95776860414a369ef2e139d22e101756134bb778
MdTasib <ohidulalam442@gmail.com>
2021-09-02 13:39:05 +0600

    env file setup

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.get('/', (req, res) => {
   res.send('Hello')
 });
 
-// const uri = "mongodb+srv://safeshop:safeshop@cluster0.do24a.mongodb.net/SafeShop?retryWrites=true&w=majority";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.do24a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("SafeShop").collection("products");
